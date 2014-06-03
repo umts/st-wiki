b168e8f91b411714300f9012541b34e381a7dd31
Matt Moretti <werebus@gmail.com>
2014-06-03 17:58:03 -0400

    Massive permissions cleanup

--- LocalSettings.php
+++ LocalSettings.php
@@ -70,61 +70,15 @@ $wgEnotifUserTalk = true; # UPO
 $wgEnotifWatchlist = true; # UPO
 $wgEmailAuthentication = true;
 
-# Schemas for Postgres
-$wgDBmwschema       = "mediawiki";
-$wgDBts2schema      = "public";
-
 # Experimental charset support for MySQL 4.1/5.0.
 $wgDBmysql5 = false;
-
-## Shared memory settings
-$wgMainCacheType = CACHE_NONE;
-$wgMemCachedServers = array();
 
 ## To enable image uploads, make sure the 'images' directory
 ## is writable, then set this to true:
 $wgEnableUploads             = true;
 $wgUseImageResize            = true;
-//$wgUseImageMagick            = true;
-//$wgImageMagickConvertCommand = "/usr/local/bin/convert";
-
-## If you want to use image uploads under safe mode,
-## create the directories images/archive, images/thumb and
-## images/temp, and make them all writable. Then uncomment
-## this, if it's not already uncommented:
-# $wgHashedUploadDirectory = false;
-
-## If you have the appropriate support software installed
-## you can enable inline LaTeX equations:
-$wgUseTeX           = false;
 
 $wgLocalInterwiki   = $wgSitename;
-
-$wgLanguageCode = "en";
-
-$wgProxyKey = "50e6ed523cbefd09afb5adfa3d02136ab0b8a2b538fee322ac93706f6cbc02c4";
-
-## For attaching licensing metadata to pages, and displaying an
-## appropriate copyright notice / icon. GNU Free Documentation
-## License and Creative Commons licenses are supported so far.
-# $wgEnableCreativeCommonsRdf = true;
-$wgRightsPage = ""; # Set to the title of a wiki page that describes your license/copyright
-$wgRightsUrl = "";
-$wgRightsText = "";
-$wgRightsIcon = "";
-# $wgRightsCode = ""; # Not yet used
-
-$wgDiff3 = "";
-
-# When you make changes to this configuration file, this will make
-# sure that cached pages are cleared.
-$configdate = gmdate( 'YmdHis', @filemtime( __FILE__ ) );
-$wgCacheEpoch = max( $wgCacheEpoch, $configdate );
-
-
-#---------------------------------------------------------------------
-# UMTS Added by Jeff 2006-01-05
-#---------------------------------------------------------------------
 
 /*
 * Default privilege settings
@@ -142,26 +96,132 @@ $wgCacheEpoch = max( $wgCacheEpoch, $configdate );
  */
 $wgGroupPermissions = array();
 
-// Merged the default privs. with what was added later to reduce code.
-
 // Default anon privileges. Implicit group for all visitors 
-$wgGroupPermissions['*'    ]['createaccount']   = false;
-$wgGroupPermissions['*'    ]['read']            = true;
-$wgGroupPermissions['*'    ]['edit']            = true; //Not really, we protect every namespace below
-$wgGroupPermissions['*'    ]['createpage']      = false;
-$wgGroupPermissions['*'    ]['edittalk']        = true; //Anon users CAN edit talk pages
-$wgGroupPermissions['*'    ]['createtalk']      = true;
+$wgGroupPermissions['*']['read']       = true;
+$wgGroupPermissions['*']['edit']       = true; //Not really, we protect every namespace below
+$wgGroupPermissions['*']['edittalk']   = true; //Anon users CAN edit talk pages
+$wgGroupPermissions['*']['createtalk'] = true;
 
-// Implicit group for all logged-in accounts.
-$wgGroupPermissions['user' ]['move']            = false;
-$wgGroupPermissions['user' ]['read']            = true;
-$wgGroupPermissions['user' ]['createpage']      = false;
-$wgGroupPermissions['user' ]['createtalk']      = true;
-$wgGroupPermissions['user' ]['upload']          = false;
-$wgGroupPermissions['user' ]['reupload']        = false;
-$wgGroupPermissions['user' ]['reupload-shared'] = false;
-$wgGroupPermissions['user' ]['minoredit']       = false;
+// Implicit group for all logged-in accounts
+$wgGroupPermissions['user'] = $wgGroupPermissions['*'];
+$wgGroupPermissions['user']['editmyusercss']     = true;
+$wgGroupPermissions['user']['editmyuserjs']      = true;
+$wgGroupPermissions['user']['viewmywatchlist']   = true;
+$wgGroupPermissions['user']['editmywatchlist']   = true;
+$wgGroupPermissions['user']['viewmyprivateinfo'] = true;
+$wgGroupPermissions['user']['editmyprivateinfo'] = true;
+$wgGroupPermissions['user']['editmyoptions']     = true;
+$wgGroupPermissions['user']['sendemail']         = true;
 
+// Implicit group for accounts that pass $wgAutoConfirmAge
+$wgGroupPermissions['autoconfirmed']['autoconfirmed'] = true;
+$wgGroupPermissions['autoconfirmed']['editsemiprotected'] = true;
+
+//group of users who have editing access
+$wgGroupPermissions['editors' ]['read']            = true;
+$wgGroupPermissions['editors' ]['edit']            = true;
+$wgGroupPermissions['editors' ]['createpage']      = true;
+$wgGroupPermissions['editors' ]['createtalk']      = true;
+$wgGroupPermissions['editors' ]['minoredit']       = true;
+$wgGroupPermissions['editors']['editpages']   	   = true;
+$wgGroupPermissions['editors']['writeapi']         = true;
+
+//create a special user group --> most access
+$wgGroupPermissions['privileged' ]['move']               = true;
+$wgGroupPermissions['privileged' ]['move-subpages']      = true;
+$wgGroupPermissions['privileged' ]['move-rootuserpages'] = true;
+$wgGroupPermissions['privileged' ]['read']               = true;
+$wgGroupPermissions['privileged' ]['edit']               = true;
+$wgGroupPermissions['privileged' ]['createpage']         = true;
+$wgGroupPermissions['privileged' ]['createtalk']         = true;
+$wgGroupPermissions['privileged' ]['upload']             = true;
+$wgGroupPermissions['privileged' ]['reupload']           = true;
+$wgGroupPermissions['privileged' ]['reupload-shared']    = true;
+$wgGroupPermissions['privileged' ]['minoredit']          = true;
+$wgGroupPermissions['privileged' ]['rollback']           = true;
+$wgGroupPermissions['privileged' ]['editmain'] 	         = true;
+$wgGroupPermissions['privileged' ]['purge']              = true; // can use ?action=purge without clicking "ok"
+
+//groups of users who can upload
+$wgGroupPermissions['uploaders' ]['upload']          = true;
+$wgGroupPermissions['uploaders' ]['reupload']        = true;
+$wgGroupPermissions['uploaders' ]['reupload-shared'] = true;
+
+// Users with bot privilege can have their edits hidden
+// from various log pages by default
+$wgGroupPermissions['bot']['bot']               = true;
+$wgGroupPermissions['bot']['autoconfirmed']     = true;
+$wgGroupPermissions['bot']['editsemiprotected'] = true;
+$wgGroupPermissions['bot']['nominornewtalk']    = true;
+$wgGroupPermissions['bot']['autopatrol']        = true;
+$wgGroupPermissions['bot']['suppressredirect']  = true;
+$wgGroupPermissions['bot']['apihighlimits']     = true;
+$wgGroupPermissions['bot']['writeapi']          = true;
+
+// Most extra permission abilities go to sysops
+$wgGroupPermissions['sysop']['block']              = true;
+$wgGroupPermissions['sysop']['createaccount']      = true;
+$wgGroupPermissions['sysop']['delete']             = true;
+// can be separately configured for pages with > $wgDeleteRevisionsLimit revs
+$wgGroupPermissions['sysop']['bigdelete']          = true;
+// can view deleted history entries, but not see or restore the text
+$wgGroupPermissions['sysop']['deletedhistory']     = true;
+$wgGroupPermissions['sysop']['deletedtext']        = true;
+$wgGroupPermissions['sysop']['undelete']           = true;
+$wgGroupPermissions['sysop']['editinterface']      = true;
+$wgGroupPermissions['sysop']['editusercss']        = true;
+$wgGroupPermissions['sysop']['edituserjs']         = true;
+$wgGroupPermissions['sysop']['import']             = true;
+$wgGroupPermissions['sysop']['importupload']       = true;
+$wgGroupPermissions['sysop']['move']               = true;
+$wgGroupPermissions['sysop']['move-subpages']      = true;
+$wgGroupPermissions['sysop']['move-rootuserpages'] = true;
+$wgGroupPermissions['sysop']['patrol']             = true;
+$wgGroupPermissions['sysop']['autopatrol']         = true;
+$wgGroupPermissions['sysop']['protect']            = true;
+$wgGroupPermissions['sysop']['editprotected']      = true;
+$wgGroupPermissions['sysop']['proxyunbannable']    = true;
+$wgGroupPermissions['sysop']['rollback']           = true;
+$wgGroupPermissions['sysop']['upload']             = true;
+$wgGroupPermissions['sysop']['reupload']           = true;
+$wgGroupPermissions['sysop']['reupload-shared']    = true;
+$wgGroupPermissions['sysop']['unwatchedpages']     = true;
+$wgGroupPermissions['sysop']['autoconfirmed']      = true;
+$wgGroupPermissions['sysop']['autoconfirmed']      = true;
+$wgGroupPermissions['sysop']['editsemiprotected']  = true;
+$wgGroupPermissions['sysop']['ipblock-exempt']     = true;
+$wgGroupPermissions['sysop']['blockemail']         = true;
+$wgGroupPermissions['sysop']['markbotedits']       = true;
+$wgGroupPermissions['sysop']['apihighlimits']      = true;
+$wgGroupPermissions['sysop']['browsearchive']      = true;
+$wgGroupPermissions['sysop']['noratelimit']        = true;
+$wgGroupPermissions['sysop']['movefile']           = true;
+$wgGroupPermissions['sysop']['unblockself']        = true;
+$wgGroupPermissions['sysop']['suppressredirect']   = true;
+
+// Permission to change users' group assignments
+$wgGroupPermissions['bureaucrat']['userrights']      = true;
+// The following are default to sysops, but given to bureacrats instead.
+$wgGroupPermissions['bureaucrat']['editinterface']   = true;
+$wgGroupPermissions['bureaucrat']['import']          = true;
+$wgGroupPermissions['bureaucrat']['importupload']    = true;
+$wgGroupPermissions['bureaucrat']['upload_by_url']   = true;
+$wgGroupPermissions['bureaucrat']['trackback']       = true;
+$wgGroupPermissions['bureaucrat']['proxyunbannable'] = true;
+$wgGroupPermissions['bureaucrat']['noratelimit']     = true;
+$wgGroupPermissions['bureaucrat']['patrol']          = true;
+
+
+// Implicit group for accounts with confirmed email addresses
+// This has little use when email address confirmation is off
+# Make it so users with confirmed e-mail addresses are in the group.
+$wgAutopromote['emailconfirmed'] = APCOND_EMAILCONFIRMED;
+# Hide group from user list. 
+$wgImplicitGroups = array( 'emailconfirmed' );
+# Finally, set it to true for the desired group.
+$wgGroupPermissions['emailconfirmed']['emailconfirmed'] = true;
+
+$wgImplicitGroups = array( '*', 'user', 'autoconfirmed', 'emailconfirmed' );
 
 //Talk pages need the 'edittalk' permission (everyone has that)
 $wgNamespaceProtection[NS_TALK] = array( 'edittalk' );
@@ -176,122 +236,35 @@ $wgNamespaceProtection[NS_MAIN] = array( 'editpages' );
 //Edit interfaces and special: only bureaucrats
 $wgNamespaceProtection[NS_SPECIAL] = $wgNamespaceProtection[NS_MEDIAWIKI] = array( 'editinterface' );
 
-// Implicit group for accounts that pass $wgAutoConfirmAge
-//$wgGroupPermissions['autoconfirmed']['autoconfirmed'] = true;
-
-// Implicit group for accounts with confirmed email addresses
-// This has little use when email address confirmation is off
-# Make it so users with confirmed e-mail addresses are in the group.
-$wgAutopromote['emailconfirmed'] = APCOND_EMAILCONFIRMED;
-# Hide group from user list. 
-$wgImplicitGroups = array( 'emailconfirmed' );
-# Finally, set it to true for the desired group.
-$wgGroupPermissions['emailconfirmed']['emailconfirmed'] = true;
-
-// Users with bot privilege can have their edits hidden
-// from various log pages by default
-$wgGroupPermissions['bot'  ]['bot']             = true;
-$wgGroupPermissions['bot'  ]['autoconfirmed']   = true;
-
-// Most extra permission abilities go to sysops
-$wgGroupPermissions['sysop']['block']           = true;
-$wgGroupPermissions['sysop']['createaccount']   = true;
-$wgGroupPermissions['sysop']['delete']          = true;
-$wgGroupPermissions['sysop']['editinterface'] = true;
-
-// can view deleted history entries, but not see or restore the text
-$wgGroupPermissions['sysop']['deletedhistory']  = true;
-$wgGroupPermissions['sysop']['move']            = true;
-$wgGroupPermissions['sysop']['protect']         = true;
-$wgGroupPermissions['sysop']['rollback']        = true;
-$wgGroupPermissions['sysop']['upload']          = true;
-$wgGroupPermissions['sysop']['reupload']        = true;
-$wgGroupPermissions['sysop']['reupload-shared'] = true;
-$wgGroupPermissions['sysop']['unwatchedpages']  = true;
-$wgGroupPermissions['sysop']['autoconfirmed']   = true;
-
-// To hide usernames from users and Sysops
-// Added in 1.10.0, but not added into DefaultSettings.php until 1.13.0
-$wgGroupPermissions['suppress']['hideuser'] = true;
-
-// Permission to change users' group assignments
-$wgGroupPermissions['bureaucrat']['userrights'] = true;
-
-// The following are default to sysops, but given to bureacrats instead.
-$wgGroupPermissions['bureaucrat']['editinterface']   = true;
-$wgGroupPermissions['bureaucrat']['import']          = true;
-$wgGroupPermissions['bureaucrat']['importupload']    = true;
-$wgGroupPermissions['bureaucrat']['upload_by_url']   = true;
-$wgGroupPermissions['bureaucrat']['trackback']       = true;
-$wgGroupPermissions['bureaucrat']['proxyunbannable'] = true;
-$wgGroupPermissions['bureaucrat']['patrol']          = true;
-
-#----------------------------------------------------------------
-#  Added Groups
-#----------------------------------------------------------------
-
-//create a special user group --> most access
-$wgGroupPermissions['privileged' ]['move']            = true;
-$wgGroupPermissions['privileged' ]['read']            = true;
-$wgGroupPermissions['privileged' ]['edit']            = true;
-$wgGroupPermissions['privileged' ]['createpage']      = true;
-$wgGroupPermissions['privileged' ]['createtalk']      = true;
-$wgGroupPermissions['privileged' ]['upload']          = true;
-$wgGroupPermissions['privileged' ]['reupload']        = true;
-$wgGroupPermissions['privileged' ]['reupload-shared'] = true;
-$wgGroupPermissions['privileged' ]['minoredit']       = true;
-$wgGroupPermissions['privileged' ]['rollback']        = true;
-$wgGroupPermissions['privileged' ]['editmain'] 	      = true;
-
-//group of users who have editing access
-$wgGroupPermissions['editors' ]['move']            = false;
-$wgGroupPermissions['editors' ]['read']            = true;
-$wgGroupPermissions['editors' ]['edit']            = true;
-$wgGroupPermissions['editors' ]['createpage']      = true;
-$wgGroupPermissions['editors' ]['createtalk']      = true;
-$wgGroupPermissions['editors' ]['minoredit']       = true;
-$wgGroupPermissions['editors']['editpages'] 	   = true;
-
-//groups of users who can upload
-$wgGroupPermissions['uploaders' ]['upload']          = true;
-$wgGroupPermissions['uploaders' ]['reupload']        = true;
-$wgGroupPermissions['uploaders' ]['reupload-shared'] = true;
-
-//Will change lookupcreds to bureaucrat if extension will still work.
-$wgGroupPermissions['sysop' ]['lookupcredentials'] 	= true;
-
 //removes talk page link for non-logged-in users
 $wgShowIPinHeader = false; 
 
 //Added to support additional file types - adam 2007.01.18
 $wgFileExtensions = array( 'png', 'gif', 'emf', 'wmf', 'jpg', 'jpeg', 'pdf', 'svg');
 
-require_once $IP.'/extensions/cite/Cite.php' ;	
-require_once( $IP.'/extensions/cite/SpecialCite.php' );
+require_once( $IP.'/extensions/Cite/Cite.php' );	
+require_once( $IP.'/extensions/Cite/SpecialCite.php' );
+
 require_once( "$IP/extensions/ParserFunctions/ParserFunctions.php" );
 
-// Confirm account extension disables direct account creation and requires approval of accounts by bureaucrats. FROZEN UNTIL WM 1.11 UPDATE
-include_once( $IP.'/extensions/ConfirmAccount/SpecialConfirmAccount.php' );
+require_once( $IP.'/extensions/ConfirmAccount/ConfirmAccount.php' );
 #$wgAccountRequestTypes = array( 'readers', 'editors', 'users' );
-$wgAccountRequestTypes = array( 0 => array( 'Drivers', 'editors' ),
-                                1 => array( 'Passengers', 'users' ),
-                                2 => array( 'Staff', 'bureaucrat' ) );
-$wgConfirmAccountSaveInfo = true;
-$wgUseRealNamesOnly = false;
-$wgAccountRequestToS = false;
-$wgAccountRequestExtraInfo = false;
-$wgAccountRequestMinWords = 0;
-$wgGroupPermissions['sysop' ]['confirmaccount']		= true;
-$wgGroupPermissions['bureaucrat' ]['requestips'] 	= true;
+$wgConfirmAccountRequestFormItems['Biography']['minWords'] = 0;
+$wgConfirmAccountRequestFormItems['AreasOfInterest']['enabled'] = false;
+$wgConfirmAccountRequestFormItems['CV']['enabled'] = false;
+$wgConfirmAccountRequestFormItems['Links']['enabled'] = false;
 
-//To enable Captchas/Confirm Edit extensions. -1/3/08 LB2
+$wgAccountRequestTypes = array( 0 => array( 'Drivers', 'editors' ),
+                                1 => array( 'Passengers', 'user' ),
+                                2 => array( 'Staff', 'bureaucrat' ) );
+
 //Keys are in the Sensitive file.
 require_once( "$IP/extensions/ConfirmEdit/ConfirmEdit.php" );
-require_once("$IP/extensions/ConfirmEdit/ReCaptcha.php");
+require_once( "$IP/extensions/ConfirmEdit/ReCaptcha.php" );
 $wgCaptchaClass = 'ReCaptcha';
 
 $wgUseAjax = true;
-require_once("{$IP}/extensions/CategoryTree/CategoryTree.php");
+require_once( "$IP/extensions/CategoryTree/CategoryTree.php" );
 
 require_once ( "$IP/extensions/SecurePages/SecurePages.php" );
 $wgSecurePages = array(-1 => array( 'UserLogin', 'Preferences', 'ChangePassword'),);
