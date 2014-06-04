2f9ea19d5c899ee63602dcf3008564a310c7ab10
Matt Moretti <werebus@gmail.com>
2014-06-03 22:42:14 -0400

    No, seriously so SMRT

--- skins/umass/Resources.php
+++ skins/umass/Resources.php
@@ -13,7 +13,7 @@ $wgResourceModules['skins.umass'] = array(
 
 $wgResourceModules['skins.umass.js'] = array(
   'scripts' => array( 'umass/headroom.min.js',
-                      'umass/custom.js' )
+                      'umass/custom.js' ),
   'remoteBasePath' => $wgStylePath,
   'localBasePath' => $wgStyleDirectory,
  );
